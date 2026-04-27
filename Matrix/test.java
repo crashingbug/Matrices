fb310fb2d64ded086d26a02e74c85f0c4844580d
crashingbug <78635936+crashingbug@users.noreply.github.com>
2026-04-27 21:53:04 +0530

    Update test.java

--- Matrix/test.java
+++ Matrix/test.java
@@ -4,6 +4,6 @@ public class test {
     public static void main(String[] args) {
         Matrix a = new Matrix(4,4);
         a.display();
-        Matrix.display(a.Minor(1,2));
+        Matrix.display(a.Inverse());
     }
 }
